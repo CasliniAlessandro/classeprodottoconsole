d183d9302013e09a09406def1fb8a5044a7f7cd5
caslini.20353 <caslini.20353@E2-9-PC20>
2023-09-19 13:57:09 +0200

    Aggiungere i file di progetto.

---
+++ classeprodottoconsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classeprodottoconsole
+
+{
+    internal class Program
+    { 
+      static void Main(string[] args)
+        {
+            Console.Write("Inserire il nome del prodotto: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Inserire il prezzo del prodotto: ");
+            double prezzo = double.Parse(Console.ReadLine());
+
+            Prodotto p =new Prodotto(nome,prezzo);
+            Prodotto prod = new Prodotto();
+
+            nome = "null";
+            prezzo = 0;
+
+            Console.WriteLine("\nVariabile nome: " + nome);
+            Console.WriteLine("\nVariabile prezzo: " + prezzo);
+
+            Console.WriteLine("\nPrezzo attributo: " + p.gPrezzo());
+            Console.WriteLine("\nNome attributo: " + p.gNome());
+        }
+    }
+}
---
+++ classeprodottoconsole/prodotto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classeprodottoconsole
+{
+    class Prodotto
+    {
+        private string nome;
+        private double prezzo;
+
+        public Prodotto()
+        {
+            nome = "prodotto sconosciuto";
+            prezzo= 0.0;
+        }
+        public Prodotto(string nome, double prezzo)
+        {
+            this.nome = nome;   
+            this.prezzo = prezzo;   
+        }
+        public string gNome() 
+        {
+                return nome;    
+        }
+
+        public double gPrezzo() 
+        {
+            return prezzo;  
+        }
+
+
+
+    }
+}
